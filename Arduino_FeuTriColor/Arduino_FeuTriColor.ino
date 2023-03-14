9d6e603e09d6bbafc86a000aa78765e5856feb6c
Your name <you@example.com>
2023-03-14 16:30:14 +0100

    Add button before led activation

--- Arduino_FeuTriColor/Arduino_FeuTriColor.ino
+++ Arduino_FeuTriColor/Arduino_FeuTriColor.ino
@@ -1,7 +1,9 @@
-#define PIN_GREEN (uint8_t) 3
-#define PIN_YELLOW (uint8_t) 5
-#define PIN_RED (uint8_t) 6
+#define PIN_BUTTON (uint8_t)2
+#define PIN_GREEN (uint8_t)3
+#define PIN_YELLOW (uint8_t)5
+#define PIN_RED (uint8_t)6
 
+#define WAIT_TIME 1000
 #define DELAY_LED 500
 
 void writeOnLed(uint8_t PIN) {
@@ -11,13 +13,26 @@ void writeOnLed(uint8_t PIN) {
 }
 
 void setup() {
+  Serial.begin(9600);
+
+  pinMode(PIN_BUTTON, INPUT);
   pinMode(PIN_GREEN, OUTPUT);
   pinMode(PIN_YELLOW, OUTPUT);
   pinMode(PIN_RED, OUTPUT);
 }
 
 void loop() {
-  writeOnLed(PIN_GREEN);
-  writeOnLed(PIN_YELLOW);
-  writeOnLed(PIN_RED);
+  bool buttonState = digitalRead(PIN_BUTTON);
+
+  if (buttonState) {
+    writeOnLed(PIN_GREEN);
+    writeOnLed(PIN_YELLOW);
+    writeOnLed(PIN_RED);
+
+    Serial.println("LED activated");
+  } else {
+    Serial.println("Not activated");
+  }
+
+  delay(WAIT_TIME);
 }
