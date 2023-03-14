39bce435af1a7ce7d85d0d3fab67491dc05192b7
Your name <you@example.com>
2023-03-14 15:27:21 +0100

    Use end char for array of char

--- Arduino_Test.ino
+++ Arduino_Test.ino
@@ -1,14 +1,16 @@
+char inputName[15] = "No name !";
+
 /**
- * Serial waiting for read
- *
- * @param msTimeout "waiting time until user enter his name"
- */
+  * Serial waiting for read
+  *
+  * @param msTimeout "waiting time until user enter his name"
+  */
 void waitSerialUntilNotAvailable(uint16_t msTimeout = 10000) {
   uint16_t counter = 0;
   Serial.flush();
 
   while (Serial.available() == 0 && counter < msTimeout) {
-    counter+=300;
+    counter += 300;
     delay(300);
   }
 }
@@ -18,12 +20,18 @@ void setup() {
   Serial.print("Welcome to Test World !,\n Your name, please > ");
 
   waitSerialUntilNotAvailable();
-  char inputName[15] = "No name !";
 
   if (Serial.available() > 0) {
+    uint8_t sizeRead = Serial.available();
     Serial.readBytesUntil('\n', inputName, 15);
+
+    if (sizeRead < 15) {
+      inputName[sizeRead - 1] = '\0';      
+    } else {
+      inputName[14] = '\0';
+    }
+
     Serial.print("\nName chosed is > ");
-    Serial.println(inputName);
 
     // while (Serial.available()) {
     //   char inputValue = Serial.read();
@@ -32,8 +40,13 @@ void setup() {
     // }
   } else {
     Serial.println("\nNo name entered !");
-    Serial.println(inputName);
   }
+
+  Serial.println(inputName);
 }
 
-void loop() {}
+void loop() {
+  Serial.print("Welcome, ");
+  Serial.println(inputName);
+  delay(1000);
+}
