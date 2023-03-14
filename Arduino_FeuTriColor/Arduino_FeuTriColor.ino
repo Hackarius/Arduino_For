467e3586d906751f6db3afbe36c206d5b0ecc01f
Your name <you@example.com>
2023-03-14 16:14:09 +0100

    Add define macro for arduino

--- Arduino_FeuTriColor/Arduino_FeuTriColor.ino
+++ Arduino_FeuTriColor/Arduino_FeuTriColor.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#define PIN_GREEN (uint8_t) 3
+#define PIN_YELLOW (uint8_t) 5
+#define PIN_RED (uint8_t) 6
 
+#define DELAY_LED 500
+
+void writeOnLed(uint8_t PIN) {
+  digitalWrite(PIN, HIGH);
+  delay(DELAY_LED);
+  digitalWrite(PIN, LOW);
+}
+
+void setup() {
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_YELLOW, OUTPUT);
+  pinMode(PIN_RED, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  writeOnLed(PIN_GREEN);
+  writeOnLed(PIN_YELLOW);
+  writeOnLed(PIN_RED);
 }
