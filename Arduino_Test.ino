eaa7395fdd185b17124fc65b9a39a4ecbb710be5
Your name <you@example.com>
2023-03-14 15:16:39 +0100

    Add var char array for read name

--- Arduino_Test.ino
+++ Arduino_Test.ino
@@ -3,10 +3,12 @@
  *
  * @param msTimeout "waiting time until user enter his name"
  */
-void waitSerialUntilNotAvailable(uint16_t msTimeout) {
+void waitSerialUntilNotAvailable(uint16_t msTimeout = 10000) {
+  uint16_t counter = 0;
   Serial.flush();
 
-  while (Serial.available() == 0) {
+  while (Serial.available() == 0 && counter < msTimeout) {
+    counter+=300;
     delay(300);
   }
 }
@@ -15,18 +17,22 @@ void setup() {
   Serial.begin(9600);
   Serial.print("Welcome to Test World !,\n Your name, please > ");
 
-  waitSerialUntilNotAvailable(6000);
+  waitSerialUntilNotAvailable();
+  char inputName[15] = "No name !";
 
   if (Serial.available() > 0) {
-    
-    while (Serial.available()) {
-      char inputValue = Serial.read();
-      Serial.print("\nChar received > ");
-      Serial.println(inputValue);
-    }
-    
+    Serial.readBytesUntil('\n', inputName, 15);
+    Serial.print("\nName chosed is > ");
+    Serial.println(inputName);
+
+    // while (Serial.available()) {
+    //   char inputValue = Serial.read();
+    //   Serial.print("\nChar received > ");
+    //   Serial.println(inputValue);
+    // }
   } else {
     Serial.println("\nNo name entered !");
+    Serial.println(inputName);
   }
 }
 
