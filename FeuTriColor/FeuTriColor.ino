926451115880262604695ce1deccc27873e1c00b
Your name <you@example.com>
2023-03-15 10:15:08 +0100

    Add configuration for vscode

--- FeuTriColor/FeuTriColor.ino
+++ FeuTriColor/FeuTriColor.ino
@@ -6,21 +6,31 @@
 #define PIN_RED (uint8_t) 6
 
 #define MAX_VAL_POT 1024.0F
+#define MIN_VAL_POT 0.0F
 #define MAX_VOLTAGE 5
+#define MIN_VOLTAGE 0
 #define WAIT_TIME 1000
 #define DELAY_LED 500
 
-void writeOnLed(uint8_t PIN) {
-  digitalWrite(PIN, HIGH);
+#define TIME_MIN 1000
+#define TIME_MAX 10000
+
+/**
+  * Activate pin passed in arg and low after delay
+  * @param pin pin name, like PIN_RED 
+  * @return void
+  */
+void writeOnLed(uint8_t pin) {
+  digitalWrite(pin, HIGH);
   delay(DELAY_LED);
-  digitalWrite(PIN, LOW);
+  digitalWrite(pin, LOW);
 }
 
 /**
   * Convert value from potentiometer in voltage
   * @param potentiometerValue value read from the potentiometer
   * @param vRef voltage reference in the board
-  * @return float
+  * @return float return convert value in voltage
   */
 float getPotentiometerRatio(uint16_t potentiometerValue, float vRef = MAX_VOLTAGE) {
   return vRef / MAX_VAL_POT * (float)potentiometerValue;
@@ -29,9 +39,9 @@ float getPotentiometerRatio(uint16_t potentiometerValue, float vRef = MAX_VOLTAG
 /**
   * Read value from potentiometer
   * Display value from potentiometer and value in voltage
-  * @return void
+  * @return int return potentiometer without convert
   */
-void readPotentiometer() {
+int readPotentiometer() {
   int potentiometerValue = analogRead(PIN_POT);
   Serial.print("Value from potentiometer > ");
   Serial.println(potentiometerValue);
@@ -42,6 +52,8 @@ void readPotentiometer() {
   float voltValue = getPotentiometerRatio(potentiometerValue);
   Serial.print("Value in volt > ");
   Serial.println(voltValue);
+
+  return potentiometerValue;
 }
 
 void setup() {
@@ -57,13 +69,16 @@ void loop() {
   bool buttonState = digitalRead(PIN_BUTTON);
 
   if (buttonState) {
-    readPotentiometer();
+    int potValue = readPotentiometer();
+    long timeToWait = map(potValue, 0, 1024, TIME_MIN, TIME_MAX);
 
     writeOnLed(PIN_GREEN);
     writeOnLed(PIN_YELLOW);
     writeOnLed(PIN_RED);
 
     Serial.println("LED activated");
+
+    delay(timeToWait);
   } else {
     Serial.println("Not activated");
 
