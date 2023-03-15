97c926f8b58506a25c04ad8d06dee3c108da87ae
Your name <you@example.com>
2023-03-15 14:00:40 +0100

    Display information on LCD, and create new char

--- src/LCD/LCD.ino
+++ src/LCD/LCD.ino
@@ -1,64 +1,70 @@
-/*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
-
-*/
-
-// include the library code:
 #include <LiquidCrystal.h>
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
+#define PIN_VOLTMETER A0
+
+#define R1 2000
+#define R2 560
+
+#define MAX_VOLTAGE 22.857F
+#define DEFAULT_VOLTAGE 5.0F
+#define RATIO_VOLTAGE (float)(MAX_VOLTAGE/DEFAULT_VOLTAGE)
+#define RATIO_ADC (float)(DEFAULT_VOLTAGE/1024.0F)
+
+#define WAIT_TIME 100
+
+const byte CARAC_E_ACC_GRAVE[8] = {
+    B01000,
+    B00100,
+    B01110,
+    B10001,
+    B11111,
+    B10000,
+    B01110,
+    B00000
+};
+
+const byte CARAC_OHM[8] = {
+    B00000,
+    B00000,
+    B01110,
+    B10001,
+    B01010,
+    B11011,
+    B00000,
+    B00000
+};
+
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+float getResultVIn(uint16_t vAdc) {
+    return RATIO_ADC * vAdc * RATIO_VOLTAGE;
 }
 
+void setup() {
+    lcd.createChar(0, CARAC_E_ACC_GRAVE);
+    lcd.createChar(1, CARAC_OHM);
+
+    lcd.begin(16, 2);
+    lcd.print("Orsys");
+    lcd.setCursor(0, 1);
+    lcd.print("Voltm");
+    lcd.write((byte) 0);
+    lcd.print("tre");
+
+    delay(2000);
+    lcd.clear();
+
+    lcd.setCursor(0, 0);
+    lcd.print("Voltm");
+    lcd.write((byte) 0);
+    lcd.print("tre :");
+}  
+
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+    lcd.setCursor(0, 1);
+    lcd.print(getResultVIn(analogRead(PIN_VOLTMETER)));
+    lcd.print(" "),
+    lcd.write((byte) 1);
+    delay(WAIT_TIME);
 }
