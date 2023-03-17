1046ad4a428247da4d039ee3b6428fa1d21496fb
Your name <you@example.com>
2023-03-17 14:00:22 +0100

    Add union strcuture

--- src/Manage_Communication/Manage_Communication.ino
+++ src/Manage_Communication/Manage_Communication.ino
@@ -1,9 +1,46 @@
-void setup(void)
-{
+#include "Structure.h"
+struct Datas sdata;
 
+void setup() {
+    Serial.begin(9600);
+
+    #ifdef EMITTER
+        setupValues();
+    #endif
 }
 
-void loop(void) 
-{
+void loop() {
+    #ifdef EMITTER
+        emit();
+    #endif
 
+    #ifdef RECIEVER
+        recieve();
+    #endif
 }
+
+#ifdef EMITTER
+    void setupValues() 
+    {
+        sdata.humidity = 30;
+        sdata.temperature = 22.87F;
+        sdata.message = "Hello Mister";
+    }
+
+    void emit() 
+    {
+        U_Datas ud;
+        ud.datas = sdata;
+
+        for (int i = 0; i < sizeof(struct Datas); i++) {
+            Serial.write(ud.data8[i]);
+        }
+    }
+#endif
+
+#ifdef RECIEVER
+    void recieve()
+    {
+        
+    }
+#endif
